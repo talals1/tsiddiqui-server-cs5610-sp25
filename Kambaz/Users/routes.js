83bc779a2faa2e1ad67369a8d8de3fd9f1b8acf5
Talal Siddiqui <talal.siddiqui11@gmail.com>
2025-04-02 19:50:15 -0400

    Up to start of 5.3.4

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -1,5 +1,7 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+import * as courseDao from "../Courses/dao.js";
+// let currentUser = null;
+
 export default function UserRoutes(app) {
   const createUser = (req, res) => { };
   const deleteUser = (req, res) => { };
@@ -9,7 +11,8 @@ export default function UserRoutes(app) {
     const userId = req.params.userId;
     const userUpdates = req.body;
     dao.updateUser(userId, userUpdates);
-    currentUser = dao.findUserById(userId);
+    const currentUser = dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
 
@@ -20,27 +23,54 @@ export default function UserRoutes(app) {
         { message: "Username already in use" });
       return;
     }
-    currentUser = dao.createUser(req.body);
+    const currentUser = dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
+
     res.json(currentUser);
   };
 
   const signin = (req, res) => {
     const { username, password } = req.body;
-    currentUser = dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+    const currentUser = dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
+
   };
 
   const signout = (req, res) => {
-    currentUser = null;
+    // currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
 
   const profile = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     res.json(currentUser);
   };
 
-
-
+  const findCoursesForEnrolledUser = (req, res) => {
+    let { userId } = req.params;
+    if (userId === "current") {
+      const currentUser = req.session["currentUser"];
+      if (!currentUser) {
+        res.sendStatus(401);
+        return;
+      }
+      userId = currentUser._id;
+    }
+    const courses = courseDao.findCoursesForEnrolledUser(userId);
+    res.json(courses);
+  };
+  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
+  
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
